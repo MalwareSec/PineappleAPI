366198fc51f0fc7eb679b094f16c87c55624b5d6
Malware Sec <onlinesuport.team@gmail.com>
2017-06-29 16:25:31 -0400

    initial commit

--- getClientData.py
+++ getClientData.py
@@ -1,6 +1,6 @@
 #Python 2.7
 import requests, json
-API_TOKEN="9d4e67f06446d7c4166c555b6ec6697706d054a1d04407dbfa1d69ea85f848af7dbf5316623da0adde4e697102040df8937f0b37efb174c0cccc7b4b6651fd13"
+API_TOKEN=raw_input("Enter the API Token: ")
 payload = {
 	"module":"Clients",
 	"action":"getClientData",
